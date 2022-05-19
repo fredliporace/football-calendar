d6d8bf94c144f459faad69ee8243588aba7cc92b
Frederico Liporace <liporace@amskepler.com>
2022-05-19 00:36:44 -0300

    setuptools; pre-commit; pylix fix

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# web2ical
-Generate ical files from web content
+# football-calendar
+
+Generate ical files from football fixtures from web.
+
+## Development
+
+After cloning the repository you are required to execute the following steps.
+
+Install the package with *edit* mode and with the `dev` extra:
+```bash
+$ pip install -e .[dev]
+```
+
+Install `pre-commit` to run *isort*, *pylint*, *pydocstring*, *black* and *mypy* when committing new code.
+```bash
+$ pre-commit install
+```
--- prototypes/flu.py
+++ prototypes/flu.py
@@ -1,16 +1,18 @@
 """Prototype obtaining data from ESPN page and creating ICAL for Flu matches."""
 
-from datetime import datetime, timedelta, timezone, date
-from typing import Optional, List
+from datetime import datetime, timedelta, timezone
+from typing import List, Optional
 
 import requests
-from bs4 import BeautifulSoup
-from pydantic import BaseModel
 from babel.dates import get_month_names
+from bs4 import BeautifulSoup
 from icalendar import Calendar, Event
+from pydantic import BaseModel
 
-class Match(BaseModel):
+
+class Match(BaseModel):  # pylint: disable=too-few-public-methods
     """A match."""
+
     home_team: str
     away_team: str
     dt_start: datetime
@@ -23,6 +25,7 @@ class Match(BaseModel):
     def time_defined(self) -> bool:
         """True if match has time defined, false if only the day is defined."""
         return self.dt_end is not None
+
 
 def get_matches(url: str, delta_hour: int) -> List[Match]:
     """Obtain matches by parsing URL."""
@@ -37,28 +40,49 @@ def get_matches(url: str, delta_hour: int) -> List[Match]:
         for tri in trs:
             tds = tri.find_all("td")
             if len(tds) == 7:
-                month = list(get_month_names("abbreviated", locale="pt_BR").values()).index(tds[0].text.split(" ")[2]) + 1
+                month = (
+                    list(get_month_names("abbreviated", locale="pt_BR").values()).index(
+                        tds[0].text.split(" ")[2]
+                    )
+                    + 1
+                )
                 day = int(tds[0].text.split(" ")[1])
                 hour_minute = tds[4].text.split(":")
                 if len(hour_minute) != 2:
                     # No time defined yet, adding as a full day event
-                    dt_start = datetime(year=year,month=month,day=day)
-                    matches.append(Match(home_team=tds[1].text, away_team=tds[3].text,
-                                         dt_start=dt_start,
-                                         comments=tds[5].text))
+                    dt_start = datetime(year=year, month=month, day=day)
+                    matches.append(
+                        Match(
+                            home_team=tds[1].text,
+                            away_team=tds[3].text,
+                            dt_start=dt_start,
+                            comments=tds[5].text,
+                        )
+                    )
 
                 else:
-                    dt_start=datetime(year=year, month=month, day=day, hour=int(hour_minute[0]) + delta_hour,
-                                      minute=int(hour_minute[1]),
-                                      # Obtaining local timezone: https://stackoverflow.com/a/39079819/1259982
-                                      tzinfo=datetime.now(timezone.utc).astimezone().tzinfo)
-                    matches.append(Match(home_team=tds[1].text, away_team=tds[3].text,
-                                         dt_start=dt_start,
-                                         dt_end=dt_start + timedelta(minutes=105),
-                                         comments=tds[5].text))
-                #print(f"\t{tds[0].text} {tds[1].text} {tds[3].text} {tds[4].text} {tds[5].text}")
-                #print(match)
+                    dt_start = datetime(
+                        year=year,
+                        month=month,
+                        day=day,
+                        hour=int(hour_minute[0]) + delta_hour,
+                        minute=int(hour_minute[1]),
+                        # Obtaining local timezone: https://stackoverflow.com/a/39079819/1259982
+                        tzinfo=datetime.now(timezone.utc).astimezone().tzinfo,
+                    )
+                    matches.append(
+                        Match(
+                            home_team=tds[1].text,
+                            away_team=tds[3].text,
+                            dt_start=dt_start,
+                            dt_end=dt_start + timedelta(minutes=105),
+                            comments=tds[5].text,
+                        )
+                    )
+                # print(f"\t{tds[0].text} {tds[1].text} {tds[3].text} {tds[4].text} {tds[5].text}")
+                # print(match)
     return matches
+
 
 def build_calendar(matches: List[Match]) -> Calendar:
     """Build calendar from matches."""
@@ -78,10 +102,13 @@ def build_calendar(matches: List[Match]) -> Calendar:
     # cal.add_component(event)
     return cal
 
+
 if __name__ == "__main__":
-    matches = get_matches(url="https://www.espn.com.br/futebol/time/calendario/_/id/3445/fluminense",
-                          delta_hour=1)
-    print(matches)
-    calendar = build_calendar(matches)
-    with open("fluminense.ics", "w") as fp_ical:
+    my_matches = get_matches(
+        url="https://www.espn.com.br/futebol/time/calendario/_/id/3445/fluminense",
+        delta_hour=1,
+    )
+    print(my_matches)
+    calendar = build_calendar(my_matches)
+    with open("fluminense.ics", "w", encoding="utf-8") as fp_ical:
         fp_ical.write(calendar.to_ical().decode("utf-8"))
